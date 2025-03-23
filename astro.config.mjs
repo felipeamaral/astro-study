86f553361fe040c25dc5c130c1a093fce3e51141
Felipe Amaral <amaral.felipeaugusto@gmail.com>
2025-03-23 11:58:52 -0300

    adding rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://tiny-tulumba-fdf1de.netlify.app/"
+});
