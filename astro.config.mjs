450643aaf7cc4a1461b8970fc86654d8a616cf19
Felipe Amaral <amaral.felipeaugusto@gmail.com>
2025-03-23 12:10:59 -0300

    done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://tiny-tulumba-fdf1de.netlify.app/"
+  site: "https://tiny-tulumba-fdf1de.netlify.app/",
+  integrations: [preact()]
 });
